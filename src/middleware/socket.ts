e9f5703c097f1c13ebab1a445eccad3dc7fc662e
Peliah <pelepoupa@gmail.com>
2025-10-16 10:46:11 +0100

    chore: removed boilerplate middleware

--- src/middleware/socket.ts
+++
@@ -1,9 +0,0 @@
-import type { NextFunction, Request, Response } from 'express';
-import { io } from '@/server';
-
-const attachSocket = (req: Request, res: Response, next: NextFunction) => {
-  req.io = io;
-  next();
-};
-
-export default attachSocket;
